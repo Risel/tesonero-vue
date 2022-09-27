0e926d71bd61ffdeab4dbdea3da03e1bdd4afd20
Risel <roman.ignatowi413@gmail.com>
2022-09-27 15:33:43 +0300

    Added base structure, downloaded assets, created css variables

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  hello
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
